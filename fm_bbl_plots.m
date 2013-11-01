76e31694525c05385611ee8d6248d1d7e9c08176
Romaric Verney <romaric.verney@ifremer.fr>
2013-11-01 20:00:59 +0100

    adding new plots in fm_bbl_plots

--- fm_bbl_plots.m
+++ fm_bbl_plots.m
@@ -109,6 +109,96 @@ colormap(cmap)
 set(gca,'YDir','Normal','XTick',(tmin:(tmax-tmin)/12:tmax),'XTickLabel',datestr((tmin:(tmax-tmin)/12:tmax),dts),'FontSize',12);
 caxis([-2 0])
 colorbar('Location','East','YTick',(-3:1:0),'YTickLabel',{'0.001';'0.01';'0.1';'1'})
+
+ik=find(Df>d50s,1,'first');
+Cvs_Df=zeros(size(Cmud));
+Cvs_Df(:,ik-1,:)=Cvs(:,1,:)*rhos*(Df(ik)-d50s)/(Df(ik)-Df(ik-1));
+Cvs_Df(:,ik,:)=Cvs(:,1,:)*rhos*(1-(Df(ik)-d50s)/(Df(ik)-Df(ik-1)));
+
+
+figure(3); clf; subplot(311)
+plot(t2,uw,'-b',t2,uc,'-r')
+axis([tmin tmax 0 0.4])
+set(gca,'YDir','Normal','XTick',(tmin:(tmax-tmin)/12:tmax),'XTickLabel',datestr((tmin:(tmax-tmin)/12:tmax),dts),'FontSize',12);
+legend('Uw','Uc')
+   
+   
+figure
+subplot(311)
+imagesc(t2,(1:npmud),squeeze(Cmud(6,:,:)+Cvs_Df(6,:,:))) % 0.5mab
+title('FLOCMOD sand/mud 0.55mab -- FSD','FontSize',fs)
+axis([tmin tmax 0 npmud])
+colormap(cmap)
+caxis([0 0.05])
+set(gca,'YDir','Normal','XTick',(tmin:(tmax-tmin)/12:tmax),'YTick',(1:3:npmud),'YTickLabel',num2str(1e6*Df(1:3:npmud),'%3.0f'),'XTickLabel',datestr((tmin:(tmax-tmin)/12:tmax),dts),'FontSize',12);
+% colorbar('Location','East','YTick',(1:3:np),'YTickLabel',num2str(1e6*Df(1:3:np),'%3.0f'))
+
+subplot(312)
+%bar(t2,squeeze(C(floor(nzc/2),:,:)+Cvs_Df(floor(nzc/2),:,:))','Stack')
+imagesc(t2,(1:npmud),squeeze(Cmud(3,:,:)+Cvs_Df(3,:,:))) % 0.25 mab
+title('FLOCMOD sand/mud 0.25mab -- FSD','FontSize',fs)
+axis([tmin tmax 0 npmud])
+colormap(cmap)
+caxis([0 0.05])
+set(gca,'YDir','Normal','XTick',(tmin:(tmax-tmin)/12:tmax),'YTick',(1:3:npmud),'YTickLabel',num2str(1e6*Df(1:3:npmud),'%3.0f'),'XTickLabel',datestr((tmin:(tmax-tmin)/12:tmax),dts),'FontSize',12);
+% colorbar('Location','East','YTick',(1:3:np),'YTickLabel',num2str(1e6*Df(1:3:np),'%3.0f'))
+
+subplot(313)
+imagesc(t2,(1:npmud),squeeze(Cmud(1,:,:)+Cvs_Df(1,:,:))) % 0.05mab
+title('FLOCMOD Settling 0.05mab -- FSD','FontSize',fs)
+axis([tmin tmax 0 npmud])
+colormap(cmap)
+caxis([0 1])
+set(gca,'YDir','Normal','XTick',(tmin:(tmax-tmin)/12:tmax),'YTick',(1:3:npmud),'YTickLabel',num2str(1e6*Df(1:3:npmud),'%3.0f'),'XTickLabel',datestr((tmin:(tmax-tmin)/12:tmax),dts),'FontSize',12);
+% colorbar('Location','East','YTick',(1:3:np),'YTickLabel',num2str(1e6*Df(1:3:np),'%3.0f'))
+
+
+
+
+%% plot specific events
+
+specevent=[3600/86400 3*3600/86400 4*3600/86400];
+nspec=length(specevent);
+fs=14;
+size(Cvs_Df)
+figure
+for i=1:nspec
+    tspec=find(t2>specevent(i),1,'first');
+    
+subplot(3,nspec,i)
+hold on
+plot(1e6*Df,squeeze(Cmud(6,:,tspec)),'r','LineWidth',2)
+plot(1e6*Df,squeeze(Cvs_Df(6,:,tspec)),'b','LineWidth',2)
+plot(1e6*Df,squeeze(Cmud(6,:,tspec)+Cvs_Df(6,:,tspec)),'k','LineWidth',2)
+axis([4 1500 0 0.2])
+xlabel('Particle size (um)','FontSize',fs); ylabel('Concentration (g/l)','FontSize',fs);
+if i==1; legend('mud flocs','sand','total'); ylabel({'0.5mab';'Concentration (g/l)'},'FontSize',fs);end
+title(['date=' datestr(t2(tspec),13)],'FontSize',fs)
+set(gca,'XScale','log','FontSize',12)
+
+subplot(3,nspec,i+nspec)
+hold on
+plot(1e6*Df,squeeze(Cmud(3,:,tspec)),'r','LineWidth',2)
+plot(1e6*Df,squeeze(Cvs_Df(3,:,tspec)),'b','LineWidth',2)
+plot(1e6*Df,squeeze(Cmud(3,:,tspec)+Cvs_Df(3,:,tspec)),'k','LineWidth',2)
+axis([4 1500 0 0.2])
+set(gca,'XScale','log','FontSize',12)
+xlabel('Particle size (um)','FontSize',fs); ylabel('Concentration (g/l)','FontSize',fs);
+if i==1; ylabel({'0.25mab';'Concentration (g/l)'},'FontSize',fs);end
+
+subplot(3,nspec,i+2*nspec)
+hold on
+plot(1e6*Df,squeeze(Cmud(1,:,tspec)),'r','LineWidth',2)
+plot(1e6*Df,squeeze(Cvs_Df(1,:,tspec)),'b','LineWidth',2)
+plot(1e6*Df,squeeze(Cmud(1,:,tspec)+Cvs_Df(1,:,tspec)),'k','LineWidth',2)
+axis([4 1500 0 0.2])
+set(gca,'XScale','log','FontSize',12)
+xlabel('Particle size (um)','FontSize',fs); ylabel('Concentration (g/l)','FontSize',fs);
+if i==1; ylabel({'0.05mab';'Concentration (g/l)'},'FontSize',fs);end
+end
+
 toc
+
+
 
 % profile viewer
